dc5e47b96bbd1d9d764ecd4c4860932e85ab6a4d
joelleneyap <162358269+joelleneyap@users.noreply.github.com>
2024-05-02 13:48:20 -0700

    Update datasets expanded.sql snowflake

--- datasets expanded.sql
+++ datasets expanded.sql
@@ -4,7 +4,7 @@ WITH ExpandedTags AS (
         d.id AS d_id,
         t.value::STRING AS tag_id
     FROM
-        dashboards d,
+        datasets d,
         LATERAL FLATTEN(input => PARSE_JSON(d.asset_governed_tags):governedTagIds) t
 ),
 TagsWithNames AS (
@@ -20,7 +20,7 @@ FollowersExpanded AS (
         d.id AS d_id,
         t.value::STRING AS user_id
     FROM
-        dashboards d,
+        datasets d,
         LATERAL FLATTEN(input => PARSE_JSON(d.asset_followers):followedBy) t
 ),
 FollowersWithEmails AS (
@@ -33,17 +33,17 @@ FollowersWithEmails AS (
     GROUP BY
         fe.d_id
 ),
-AssetContactsExpanded AS (
-    SELECT
+AssetContactsExpanded as (
+	SELECT
         d.id AS d_id,
         PARSE_JSON(contact.value):designation::STRING AS designation_id,
         PARSE_JSON(contact.value):value::STRING AS value_id
     FROM
-        dashboards d,
+        datasets d,
         LATERAL FLATTEN(input => PARSE_JSON(d.asset_contacts):contacts) contact
 ),
-AssetContactInfo AS (
-    SELECT
+AssetContactInfo as (
+	SELECT
         d.id AS d_id,
         ARRAY_AGG(
             OBJECT_CONSTRUCT(
@@ -52,7 +52,7 @@ AssetContactInfo AS (
             )
         ) AS contacts_info
     FROM
-        dashboards d,
+        datasets d,
         LATERAL FLATTEN(input => PARSE_JSON(d.asset_contacts):contacts) contact
         JOIN user_defined_resource u ON PARSE_JSON(contact.value):designation::STRING = u.id
         JOIN users ON PARSE_JSON(contact.value):value::STRING = users.id
@@ -60,34 +60,36 @@ AssetContactInfo AS (
         d.id
 )
 SELECT
-    d.id, 
-    d.entity_type, 
-    d._created_at, 
-    d.last_modified_at, 
-    d.last_ingested_at, 
-    d.source_info, 
-    d.logical_id, 
-    d.entity_upstream, 
-    d.dashboard_info, 
-    PARSE_JSON(d.dashboard_info):description::STRING as dashboard_info_description,
-    PARSE_JSON(d.dashboard_info):powerBi.powerBiDashboardType::STRING as dashboard_powerBI,
-    d.asset_followers, 
-    PARSE_JSON(d.asset_followers):followedBy::STRING as asset_followers_followedBy,
-    fe.follower_emails as asset_followers_emails,
-    d.asset_contacts, 
-    PARSE_JSON(d.asset_contacts):contacts::STRING as asset_contacts_contacts,
-    aci.contacts_info as asset_contacts_info,
-    d.asset_governed_tags, 
-    PARSE_JSON(d.asset_governed_tags):governedTagIds::STRING as asset_governed_tags_governedTagIds,
-    ARRAY_AGG(tn.tag_name) OVER (PARTITION BY tn.d_id) AS governedTagIds_tag_names,
-    PARSE_JSON(d.structure):name::STRING as structure_name,
-    PARSE_JSON(d.structure):directories::STRING as structure_directories
+	d.id,
+	d.entity_type,
+	d._created_at,
+	d.last_modified_at,
+	d.last_ingested_at,
+	PARSE_JSON(d.logical_id):name::STRING as logical_id_name,
+	PARSE_JSON(d.logical_id):platform::STRING as logical_id_platform,
+	PARSE_JSON(d.schema):description::STRING as schema_description,
+	PARSE_JSON(d.schema):entityId::STRING as schema_entityId,
+	PARSE_JSON(d.schema):fields::STRING as schema_fields,
+	PARSE_JSON(d.schema):sqlSchema.materialization::STRING as schema_sqlSchema_materialization,
+	PARSE_JSON(d.structure):database::STRING as structure_database,
+	PARSE_JSON(d.structure):schema::STRING as structure_schema,
+	PARSE_JSON(d.structure):table::STRING as structure_table,
+	PARSE_JSON(d.statistics):dataSizeBytes::STRING as statistics_dataSizeBytes,
+	PARSE_JSON(d.statistics):lastUpdated::STRING as statistics_lastUpdated,
+	PARSE_JSON(d.statistics):recordCount::STRING as statistics_recordCount,
+	PARSE_JSON(d.source_info):mainUrl::STRING as source_info_mainUrl,
+	PARSE_JSON(d.entity_upstream):sourceDatasets::STRING as upstream_sourceDatasets,
+	PARSE_JSON(d.asset_followers):followedBy::STRING as asset_followers_followedBy,
+	fe.follower_emails as asset_followers_emails,
+	PARSE_JSON(d.asset_contacts):contacts::STRING as asset_contacts_contacts,
+	aci.contacts_info as asset_contacts_info,
+	PARSE_JSON(d.asset_governed_tags):governedTagIds::STRING as asset_governed_tags_governedTagIds,
+	ARRAY_AGG(tn.tag_name) OVER (PARTITION BY tn.d_id) AS governedTagIds_tag_names
 FROM
-    dashboards d
-    LEFT JOIN TagsWithNames tn ON d.id = tn.d_id
-    LEFT JOIN FollowersWithEmails fe ON d.id = fe.d_id
-    LEFT JOIN AssetContactInfo aci ON d.id = aci.d_id;
-
+	datasets d
+	LEFT JOIN TagsWithNames tn ON d.id = tn.d_id
+	LEFT JOIN FollowersWithEmails fe ON d.id = fe.d_id
+    	LEFT JOIN AssetContactInfo aci ON d.id = aci.d_id;
 
 
 
